cf1a41f1e652f16c6536b083c83e23deddedbebc
Jake <94998878+jakebark@users.noreply.github.com>
2026-01-28 21:07:24 +0000

    validate root var (#1)
    validate root var

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 module "ou_1" {
-  for_each        = var.organizational_units
-  source          = "./modules/ou1"
-  ou_name         = each.key
-  child_ou_names  = each.value
-  root_ou_id      = var.root_ou_id
+  for_each       = var.organizational_units
+  source         = "./modules/ou1"
+  ou_name        = each.key
+  child_ou_names = each.value
+  root_ou_id     = var.root_ou_id
 }
--- variables.tf
+++ variables.tf
@@ -1,8 +1,14 @@
 variable "root_ou_id" {
   description = "root OU id, eg r-abcd"
   type        = string
+
+  validation {
+    condition     = can(regex("^r-[0-9a-z]{4,32}$", var.root_ou_id))
+    error_message = "Root OU ID must be in format 'r-' followed by 4-32 alphanumeric characters."
+  }
 }
 
 variable "organizational_units" {
-  type = map(any)
+  description = "Map of organizational units and their nested structure"
+  type        = map(any)
 }
